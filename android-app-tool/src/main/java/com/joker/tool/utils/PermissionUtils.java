fbaf24458667eb2823a8e033fe7113c12bbf465c
Dragonwrath <425137849@qq.com>
2017-06-30 11:20:20 +0800

    android-app-root-modify some code

--- android-app-tool/src/main/java/com/joker/tool/utils/PermissionUtils.java
+++ android-app-tool/src/main/java/com/joker/tool/utils/PermissionUtils.java
@@ -10,37 +10,58 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 
 import java.util.Arrays;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-//参考网易新闻逻辑
+//TODO 需要根据APP的详细逻辑设计进行相应的权限判断，该类不是通用方法，仅仅只提供参考
 public final class PermissionUtils {
 
-    private static Lock mLock = new ReentrantLock();
     private PermissionUtils() {
         throw new AssertionError("This class can not be instantiated");
     }
 
-    public static void requestPermissionForFirstTime(Context context, int requestCode ,String[] permissions) {
+    public static void requestPermissionForFirstTime(Activity activity, int requestCode ,String[] permissions) {
         try {
-            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_CONFIGURATIONS);
-            SharedPreferences config = context.getSharedPreferences("config", Context.MODE_PRIVATE);
+            PackageInfo info = activity.getPackageManager().getPackageInfo(activity.getPackageName(), PackageManager.GET_CONFIGURATIONS);
+            SharedPreferences config = activity.getSharedPreferences("config", Context.MODE_PRIVATE);
             //TODO 是否需要外部判断
             int versionCode = config.getInt("versionCode", -1);
             if (versionCode != info.versionCode) { //判断版本号是否相同，确定是否是新版本首次进入
-                requestPermission(context, requestCode, permissions);
+                requestPermission(activity, requestCode, permissions);
             }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static void requestPermission(Context context, int requestCode ,String[] permissions) {
+    public static void requestPermission(Activity activity, int requestCode ,String[] permissions) {
         //存在并发修改问题，使用BlockingQueue保证迭代修改正确性，
-        // 初始化的size，应该根据需要申请的权限数量修改
+        //初始化的size，应该根据需要申请的权限数量修改
+        ArrayBlockingQueue<String> list = new ArrayBlockingQueue<>(10);
+        list.addAll(Arrays.asList(permissions));
+        for (String permission : list) {
+            if (ActivityCompat.checkSelfPermission(activity,permission) == PackageManager.PERMISSION_GRANTED) {
+                list.remove(permission);
+            }
+        }
+        if (list.size() >= 1) {
+            String[] array = new String[list.size()];
+            list.toArray(array);
+            ActivityCompat.requestPermissions(activity, array, requestCode);
+        }
+    }
+
+    public static void openSettingForPermission(Activity activity, int requestCode) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+        ActivityCompat.startActivityForResult(activity, intent, requestCode, null);
+    }
+
+    public static void requestPermission(Fragment fragment, int requestCode, String[] permissions) {
+        Context context = fragment.getContext();
         ArrayBlockingQueue<String> list = new ArrayBlockingQueue<>(10);
         list.addAll(Arrays.asList(permissions));
         for (String permission : list) {
@@ -51,18 +72,14 @@ public final class PermissionUtils {
         if (list.size() >= 1) {
             String[] array = new String[list.size()];
             list.toArray(array);
-            ActivityCompat.requestPermissions((Activity) context, array, requestCode);
+            fragment.requestPermissions(array,requestCode);
         }
     }
 
-    public static void openSettingForPermission(Context context, int requestCode) {
-        Activity activity = (Activity) context;
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.fromParts("package", context.getPackageName(), null));
-        ActivityCompat.startActivityForResult(activity, intent, requestCode, null);
-
+    public static void openSettingForPermission(Fragment fragment, int requestCode) {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package",fragment.getContext().getPackageName(),null));
+        fragment.startActivityForResult(intent, requestCode);
     }
-
 
 }
