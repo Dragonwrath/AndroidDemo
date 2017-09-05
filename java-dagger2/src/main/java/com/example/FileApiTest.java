0a5126da096f929b9eb59718e839bf227d585ede
Dragonwrath <425137849@qq.com>
2017-09-05 17:18:20 +0800

    remove branch and modify name of java module

--- java-dagger2/src/main/java/com/example/FileApiTest.java
+++ java-dagger2/src/main/java/com/example/FileApiTest.java
@@ -3,16 +3,16 @@ package com.example;
 import java.io.File;
 import java.io.IOException;
 
-import javax.annotation.concurrent.ThreadSafe;
 
 
 public class FileApiTest {
     public static void main(String[] args) throws IOException {
         File file = new File("D:");
         File[] files = file.listFiles();
-        for (File dest : files) {
-            System.out.println("dest.getAbsolutePath() = " + dest.getAbsolutePath());
-        }
+        if (files != null)
+            for (File dest : files) {
+                System.out.println("dest.getAbsolutePath() = " + dest.getAbsolutePath());
+            }
 
         File test = new File(file, "deleteTest");
         boolean newFile = test.createNewFile();
