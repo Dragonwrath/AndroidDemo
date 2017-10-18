8c8b30ed191ba1070020c9e32102cde6e7c6ba7c
Dragonwrath <zhuning-916@163.com>
2017-10-18 15:33:26 +0800

    remove some redundant modules

--- android-mvp-nucleus/src/main/java/com/joke/nuclesusdemo/ResponseCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.joke.nuclesusdemo;
-
-
-import android.os.Bundle;
-
-public interface ResponseCallBack{
-    public void onDataSuccess(int action, int from, Bundle bundle);
-    public void onDataFailure(int action, String error);
-}
