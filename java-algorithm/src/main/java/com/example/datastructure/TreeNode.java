ba6bb0fc405e12eaa562b16ee6cf53cc1e644343
Dragonwrath <zhuning-916@163.com>
2017-11-17 17:09:19 +0800

    calculate an interview sample of shortest path of tree, and modify some code.

--- java-algorithm/src/main/java/com/example/datastructure/TreeNode.java
+++ java-algorithm/src/main/java/com/example/datastructure/TreeNode.java
@@ -1,73 +1,72 @@
 package com.example.datastructure;
 
 
-
 public class TreeNode<T extends Comparable>{
-    private TreeNode<T> parent;
-    private TreeNode<T> left;
-    private TreeNode<T> right;
-    private T value;
+  private TreeNode<T> parent;
+  private TreeNode<T> left;
+  private TreeNode<T> right;
+  private T value;
 
-    public TreeNode(T value) {
-        this.value = value;
+  TreeNode(T value){
+    this.value=value;
+  }
+
+  void setTreeNode(TreeNode<T> other){
+    setTreeNode(this,other);
+  }
+
+
+  @SuppressWarnings("unchecked")
+  private void setTreeNode(TreeNode<T> source,TreeNode<T> other){
+    final int i=other.value.compareTo(source.value);
+    if(i>0){
+      if(source.getRight()!=null){
+        setTreeNode(source.getRight(),other);
+      }else{
+        source.right=other;
+        other.parent=source;
+      }
+    }else if(i<0){
+      if(source.getLeft()!=null){
+        setTreeNode(source.getLeft(),other);
+      }else{
+        source.left=other;
+        other.parent=source;
+      }
     }
+  }
 
-    public void setTreeNode(TreeNode<T> other){
-        setTreeNode(this, other);
+  T getValue(){
+    return value;
+  }
+
+  private TreeNode<T> getLeft(){
+    return left;
+  }
+
+  private TreeNode<T> getRight(){
+    return right;
+  }
+
+  TreeNode<T> getParent(){
+    return parent;
+  }
+
+  final StringBuilder builder = new StringBuilder();
+
+  void printfTree(TreeNode<T> node){
+    StringBuilder builder = new StringBuilder();
+    checkNode(builder, node);
+    System.out.println(builder.toString());
+  }
+
+  private void checkNode(StringBuilder builder,TreeNode<T> node){
+    if(node.left!=null){
+      checkNode(builder,node.left);
     }
-
-
-    @SuppressWarnings("unchecked")
-    public void setTreeNode(TreeNode<T> source,TreeNode<T> other) {
-        final int i = other.value.compareTo(source.value);
-        if (i > 0) {
-            if (source.getRight() != null) {
-                setTreeNode(source.getRight(),other);
-            } else {
-                source.right = other;
-                other.parent = source;
-            }
-        } else if (i < 0) {
-            if (source.getLeft() != null) {
-                setTreeNode(source.getLeft(),other);
-            } else {
-                source.left = other;
-                other.parent = source;
-            }
-        }
+    builder.append(node.value.toString()+" ");
+    if(node.right!=null){
+      checkNode(builder,node.right);
     }
-
-    public T getValue() {
-        return value;
-    }
-
-    public TreeNode<T> getLeft() {
-        return left;
-    }
-
-    public TreeNode<T> getRight() {
-        return right;
-    }
-
-    public TreeNode<T> getParent() {
-        return parent;
-    }
-
-    public void printfTree(TreeNode<T> node) {
-        boolean printed = false;
-        if (node.getRight() != null) {
-            printfTree(node.getRight());
-        } else {
-            printed = true;
-            System.out.println(node.value);
-            System.out.println("printed = " + printed);
-        }
-        if (!printed) {
-            System.out.println(node.value);
-        }
-
-        if (node.getLeft() != null){
-            printfTree(node.getLeft());
-        }
-    }
+  }
 }
---
+++ java-algorithm/src/main/java/com/example/interview/ShortTreePath.java
@@ -0,0 +1,95 @@
+package com.example.interview;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.TreeSet;
+
+public class ShortTreePath{
+  public static void main(String[] args){
+    Random random=new Random();
+    TreeSet<TreeNode> set=new TreeSet<>();
+    TreeNode parent=new TreeNode(31);
+    set.add(parent);
+    for(int i=1;i<10;i++) {
+      int num=random.nextInt(i*31);
+      TreeNode node=new TreeNode(num);
+      parent.addChild(node);
+      set.add(node);
+    }
+    int path=Source.calculatePath(parent);
+    System.out.println("path is "+path);
+
+    for(TreeNode treeNode : set) {
+      System.out.println(treeNode.value);
+    }
+
+    parent.print();
+  }
+
+  static class Source{
+    static int calculatePath(TreeNode root){
+      if(root==null) return 0;
+      if(root.left==null) return calculatePath(root.right)+1;
+      if(root.right==null) return calculatePath(root.left)+1;
+      return Math.min(calculatePath(root.left),calculatePath(root.right))+1;
+    }
+  }
+
+  static class TreeNode implements Comparable<TreeNode>{
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value){
+      this.value=value;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public int compareTo(TreeNode node){
+      Objects.requireNonNull(node);
+      return value-node.value;
+    }
+
+    void addChild(TreeNode node){
+      int i=compareTo(node);
+      if(i>0){
+        if(left!=null){
+          left.addChild(node);
+        }else{
+          left=node;
+        }
+      }else if(i<0){
+        if(right!=null){
+          right.addChild(node);
+        }else{
+          right=node;
+        }
+      }else if(i==0){
+        if(node.left!=null){
+          left.addChild(node.left);
+        }
+        if(node.right!=null){
+
+        }
+      }
+    }
+
+    private final StringBuilder builder=new StringBuilder();
+
+    void print(){
+      checkNode(builder,this);
+      System.out.println(builder.toString());
+    }
+
+    void checkNode(StringBuilder builder,TreeNode node){
+      if(node.left!=null){
+        checkNode(builder,node.left);
+      }
+      builder.append(node.value+" ");
+      if(node.right!=null){
+        checkNode(builder,node.right);
+      }
+    }
+  }
+}
