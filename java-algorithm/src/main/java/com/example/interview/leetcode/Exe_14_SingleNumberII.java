e1bc307ac89b874092980c1cc62c2f939c36f22a
Dragonwrath <zhuning-916@163.com>
2017-11-24 22:04:30 +0800

    more leetcode exercise

--- java-algorithm/src/main/java/com/example/interview/leetcode/Exe_14_SingleNumberII.java
+++ java-algorithm/src/main/java/com/example/interview/leetcode/Exe_14_SingleNumberII.java
@@ -6,7 +6,7 @@ public class Exe_14_SingleNumberII{
   }
 
   static class Solution{
-    public int singleNumber(int[] A) {
+    public int singleNumber(int[] A){
       if(A.length==1) return 1;
       int cache=Integer.MIN_VALUE;
       int index=-1;
@@ -26,4 +26,38 @@ public class Exe_14_SingleNumberII{
       return cache;
     }
   }
+
+  /**
+   * Given an array of integers, every element appears twice except for one.
+   * Find that single one.
+   * Note:Your algorithm should have a linear runtime complexity.
+   * Could you implement it without using extra memory?
+   *
+   * @param A array of input
+   * @return the single num in array
+   */
+  public int singleNumberII(int[] A){
+    if(A.length==1) return A[0];
+    int sum=A[0];
+    for(int i=1;i<A.length;i++) {
+      sum=sum^A[i];
+    }
+    return sum;
+  }
+
+  public int singleNumberIII(int[] A){
+    if(A.length==1) return A[0];
+    int oneTime=0;
+    int twoTime=0;
+    int threeTime=0;
+    for(int i=0;i<A.length;i++) {
+      twoTime|=oneTime&A[i];
+      oneTime^=A[i];
+      threeTime=oneTime&twoTime;
+      oneTime&=~threeTime;
+      twoTime&=~threeTime;
+    }
+    return oneTime;
+  }
+
 }
---
+++ java-algorithm/src/main/java/com/example/interview/leetcode/Exe_16_GasStation.java
@@ -0,0 +1,60 @@
+package com.example.interview.leetcode;
+
+/**
+ * There are N gas stations along a circular route, where the amount of gas at station i isgas[i].
+ * You have a car with an unlimited gas tank and it costscost[i]of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+ * Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+ * Note:  The solution is guaranteed to be unique.
+ */
+public class Exe_16_GasStation{
+  public static void main(String[] args){
+
+  }
+
+  static class Solution{
+    public int canCompleteCircuit(int[] gas,int[] cost){
+      if(gas==null) return -1;
+      if(cost==null) return gas[0];
+      int sum=0;
+      out:
+      for(int i=0;i<gas.length;) {
+        for(int j=i;j<gas.length+i;j++) {
+          int cache;
+          if(j>=gas.length){
+            cache=j-gas.length;
+          }else cache=j;
+          sum=gas[cache]+sum-cost[cache];
+          if(sum<0){
+            i++;
+            sum=0;
+            continue out;
+          }
+        }
+        return i;
+      }
+      return -1;
+    }
+  }
+
+  /**
+   * 方法思路，如果从i点出发，可以跑完全程，
+   * 则从i-1点出发无法跑完全程，找出i-1点
+   */
+  static class BetterSolution{
+
+    public int canCompleteCircuit(int[] gas,int[] cost){
+      if(gas==null) return -1;
+      if(cost==null) return 0;
+      int remain=0, total=0, index=-1;
+      for(int i=0;i<gas.length;i++) {
+        total+=gas[i]-cost[i];
+        remain+=gas[i]-cost[i];
+        if(remain<0){
+          remain=0;
+          index=i;
+        }
+      }
+      return total>=0?index+1:-1;
+    }
+  }
+}
